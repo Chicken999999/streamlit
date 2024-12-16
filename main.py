e84c4c6d8ebb35ccc7dd9d5c069a031abe9dfac2
Alfaro <caalfaro1@cps.k12.il.us>
2024-12-16 11:44:51 -0600

    assingment

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import streamlit as st
-st.write("Hello, World!")
 
-st.title("My name is chicken ")
-st.header("I am 18 years old")
-st.subheader("Sub-header")
+# Set the title of the web page
+st.title("Mature Audience Only 18+")
 
-goon_count = st.button("Gooning Button")
-st.write(f"you have gooned {goon_count} times ")
+# Set a header
+game = st.header("Hancock Vs. STL Today at 7pm")
+# Text input for name
+winner = st.text_input = ("Enter who you think will win. ")
+# Dropdown menu for selecting an option
+options = ["Close Game", "Blowout"]
+result = st.selectbox("Choose an option:", options)
+# Slider for selecting a value
+points = st.slider("How many points will Hancock make", 1, 100, 50)
+# Submit button
+if st.button("Done"):
+    st.write(f"You think {winner} will win.")
+    st.write(f"You think it will be a {result}")
+    st.write(f"You think Hancock will make {points} points.")
+# Additional information
 
-st.slider("Value", 1, 100, 50)
+# Footer
+
