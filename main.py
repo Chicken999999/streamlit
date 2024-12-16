7e6fc5fcd79a7dd04d1de53385ba8cca35dd71a8
Alfaro <caalfaro1@cps.k12.il.us>
2024-12-16 11:48:15 -0600

    redo

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.title("Mature Audience Only 18+")
 # Set a header
 game = st.header("Hancock Vs. STL Today at 7pm")
 # Text input for name
-winner = st.text_input("Enter who you think will win. ")
+winner = st.text_input("Enter who you think will win:")
 # Dropdown menu for selecting an option
 options = ["Close Game", "Blowout"]
 result = st.selectbox("Choose an option:", options)
