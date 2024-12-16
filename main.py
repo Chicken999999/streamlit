ecc6dc744eb33fccd96c03671860f0174d45b079
Alfaro <caalfaro1@cps.k12.il.us>
2024-12-16 11:53:18 -0600

    fixed

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import streamlit as st
 st.title("Mature Audience Only 18+")
 
 # Set a header
-game = st.header("Hancock Vs. STL Today at 7pm")
+st.header("Hancock Vs. STL Today at 7pm")
+
 # Text input for name
-winner = st.text_input("Enter who you think will win:")
+winner = st.text_input("Who will win? ")
+
 # Dropdown menu for selecting an option
 options = ["Close Game", "Blowout"]
 result = st.selectbox("Choose an option:", options)
+
 # Slider for selecting a value
 points = st.slider("How many points will Hancock make", 1, 100, 50)
+
 # Submit button
 if st.button("Done"):
     st.write(f"You think {winner} will win.")
